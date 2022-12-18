984564f16e172a0194b613a7568cbc365c9a826d
nabigha-mogharbel <nabiga.ba.mo1998@gmail.com>
2022-12-18 21:33:36 +0200

    fixed some styling and onClick event

--- src/App.js
+++ src/App.js
@@ -7,12 +7,9 @@ class App extends Component {
       city: "tripoli",
     };
   }
-  renderState() {
-    let city=this.state.city;
-    console.log(city);
-  }
-  handleInputChange = (value) => {
-    this.setState({ city: value });
+
+  handleInputChange = (e) => {
+    this.setState({ city: e.target.value });
   };
   render() {
     return (
@@ -25,51 +22,55 @@ class App extends Component {
             onChange={this.handleInputChange}
             placeholder="Type in a city name"
           />
-          <button onClick={this.renderState}  id="btn">
+          <button onClick={() => console.log(this.state.city)} id="btn">
             Find Weather
           </button>
         </header>
         <main>
           <div id="current-weather">
-          <img src={"http://placekitten.com/250/250"} alt="kitty" />
-          <p className="title white-title">overcast clouds</p>
-          <p className="title big-title"><b>Temperature</b> 10&deg;C to 11&deg;C</p>
-          <p className="title medium-title"><b>Humidity</b> 18% <b>Pressure</b> 1008.48</p>
+            <img src={"http://placekitten.com/250/250"} alt="kitty" />
+            <p className="title white-title">overcast clouds</p>
+            <p className="title big-title">
+              <b>Temperature</b> 10&deg;C to 11&deg;C
+            </p>
+            <p className="title medium-title">
+              <b>Humidity</b> 18% <b>Pressure</b> 1008.48
+            </p>
           </div>
           <div id="all-day-weather">
             <div className="three-h-container">
-              <p className="time">3:00</p>
+              <p className="time">03:00</p>
               <img src={"http://placekitten.com/100/100"} alt="kitty" />
               <p className="temp">8&deg;C</p>
             </div>
             <div className="three-h-container">
-              <p className="time">6:00</p>
+              <p className="time">06:00</p>
               <img src={"http://placekitten.com/100/100"} alt="kitty" />
               <p className="temp">9&deg;C</p>
             </div>
             <div className="three-h-container">
-              <p className="time">9:00</p>
-              <img src={"http://placekitten.com/100/100"} alt="kitty"/>
+              <p className="time">09:00</p>
+              <img src={"http://placekitten.com/100/100"} alt="kitty" />
               <p className="temp">14&deg;C</p>
             </div>
             <div className="three-h-container">
               <p className="time">12:00</p>
-              <img src={"http://placekitten.com/100/100"} alt="kitty"/>
+              <img src={"http://placekitten.com/100/100"} alt="kitty" />
               <p className="temp">17&deg;C</p>
             </div>
             <div className="three-h-container">
               <p className="time">15:00</p>
-              <img src={"http://placekitten.com/100/100"} alt="kitty"/>
+              <img src={"http://placekitten.com/100/100"} alt="kitty" />
               <p className="temp">18&deg;C</p>
             </div>
             <div className="three-h-container">
               <p className="time">18:00</p>
-              <img src={"http://placekitten.com/100/100"} alt="kitty"/>
+              <img src={"http://placekitten.com/100/100"} alt="kitty" />
               <p className="temp">16&deg;C</p>
             </div>
             <div className="three-h-container">
-              <p className="time">21:000</p>
-              <img src={"http://placekitten.com/100/100"} alt="kitty"/>
+              <p className="time">21:00</p>
+              <img src={"http://placekitten.com/100/100"} alt="kitty" />
               <p className="temp">13&deg;C</p>
             </div>
           </div>
