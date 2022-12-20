0c6a2907f8bcf242eb2665cb8406a682a11f16db
nabigha-mogharbel <nabiga.ba.mo1998@gmail.com>
2022-12-20 16:04:04 +0200

    fetching from input, change state

--- src/App.js
+++ src/App.js
@@ -3,25 +3,54 @@ import AllDayWeather from "./components/AllDayWeather";
 import "./App.css";
 import MainWeather from "./components/MainWeather";
 import NavBar from "./components/NavBar";
-import FakeWeatherData from "./data/FakeWeather.json";
+import FakeWeather from "./data/FakeWeather.json";
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: FakeWeatherData,
+      city: "alstad",
+      data: FakeWeather,
     };
+    this.handleclick = this.handleclick.bind(this);
   }
-componentDidMount(){
-  console.log(this.state.data.list[4])
-}
+  handleclick(e) {
+    this.setState({ city: e });
+    console.log("app state", this.state.city);
+    let key = "70a28b505472e28686f6c86c0892daec";
+    let city = e;
+    let url = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&cnt=8&units=metric&appid=${key}`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        let a = data;
+        this.setState({ data: { ...a } });
+        console.log("after fetch", this.state.data);
+      })
+      .catch((err) => console.log(err));
+  }
+  requestWeather() {
+    let key = "70a28b505472e28686f6c86c0892daec";
+    let city = this.state.city;
+    let url = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&cnt=8&units=metric&appid=${key}`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        let a = data;
+        this.setState({ data: { ...a } });
+        console.log("after fetch", this.state.data);
+      })
+      .catch((err) => console.log(err));
+  }
+
   render() {
+    // this.requestWeather();
     return (
       <div className="app">
-        <NavBar />
+        <NavBar clickEvent={this.handleclick} />
         <main>
           <MainWeather
             src={this.state.data.list[4].weather[0].main.toLowerCase()}
-            desc={this.state.data.list[4].weather[0].description}
+            desc={this.state.data.city.name}
             minT={this.state.data.list[4].main.temp_min}
             maxT={this.state.data.list[4].main.temp_min}
             humidity={this.state.data.list[4].main.humidity}
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,15 @@
 import React from "react";
 class NavBar extends React.Component {
+  constructor(props){
+    super(props)
+    this.state={
+      city: 'tripoli'
+    }
+  }
+  handleChange(e){
+    this.setState({city: e.target.value})
+    console.log(e.target.value)
+  }
   render() {
     return (
       <header id="header">
@@ -8,8 +18,9 @@ class NavBar extends React.Component {
           type="text"
           className="input"
           placeholder="Type in a city name"
+          onChange={e => {this.setState({city: e.target.value}); console.log('nav state', this.state.city)}}
         />
-        <button id="btn">
+        <button id="btn" onClick={e=> {this.props.clickEvent(this.state.city)}}>
           Find Weather
         </button>
       </header>
