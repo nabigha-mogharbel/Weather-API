2e0d6ed37779bfdc82fb8defb95338ffc6d5a2b1
nabigha-mogharbel <nabiga.ba.mo1998@gmail.com>
2022-12-21 11:52:28 +0200

    error handled

--- src/App.js
+++ src/App.js
@@ -4,29 +4,64 @@ import "./App.css";
 import MainWeather from "./components/MainWeather";
 import NavBar from "./components/NavBar";
 import FakeWeather from "./data/FakeWeather.json";
+import ErrorHandler from "./components/ErrorHandler";
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       data: {},
-      isFetched:false
+      isFetched:false,
+      isError: false,
+      error: {}
     };
     this.handleclick = this.handleclick.bind(this);
+  }
+  displayError(error){
+    console.log('the error',error)
+    this.setState({error: error})
   }
   handleclick(e) {
     console.log("app state", this.state.city);
     let key = "70a28b505472e28686f6c86c0892daec";
     let city = e;
     let url = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&cnt=8&units=metric&appid=${key}`;
-    fetch(url)
+    /*fetch(url)
       .then((response) => response.json())
       .then((data) => {
         let a = data;
         this.setState({ data: { ...a } }); 
-        this.setState({ isFetched: true });
+        this.setState({ isFetched: false });
         console.log("after fetch", this.state.data);
       })
-      .catch((err) => {console.log(err); this.setState({isFetched: false})});
+      .catch((err) => {this.displayError(err); this.setState({isError: true})});*/
+
+    /*fetch(url)
+    .then( response =>{
+      if(!response.ok){
+        this.setState({isFetched: false})
+        console.log('err')
+      }
+      else{
+        this.setState({isFetched: true})
+        this.setState({data: response.json()})
+      }
+    }
+    )
+    .catch(err => this.setState({error: err}))*/
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        if(data.cod!='200'){
+          this.setState({isFetched: false, isError:true, error:data});
+          console.log(data)
+        }
+        else{
+        let a = data;
+        this.setState({ data: { ...a } }); 
+        this.setState({ isFetched: true, isError:false });
+        console.log("after fetch", this.state.data);}
+      })
+      .catch((err) => {this.displayError(err); this.setState({isError: true})});
   }
 
   render() {
@@ -43,6 +78,7 @@ class App extends Component {
             pressure={this.state.data.list[0].main.pressure}
           />}
           {this.state.isFetched && <AllDayWeather dayW={this.state.data.list.slice(1, 8)} />}
+          {this.state.isError && <ErrorHandler msg={this.state.error.message} />}
         </main>
       </div>
     );
